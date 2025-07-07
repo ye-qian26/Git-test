528962b13832d0202f6226423f5fa3335e1515f4
huanggengbiao <2864201368@qq.com>
2025-07-07 20:52:10 +0800

    第二次提交

--- src/main/java/com/yeqian/pojo/Student.java
+++ src/main/java/com/yeqian/pojo/Student.java
@@ -1,4 +1,37 @@
 package com.yeqian.pojo;
 
 public class Student {
+    private Long id;
+    private String name;
+    private String age;
+
+    public Student(Long id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
